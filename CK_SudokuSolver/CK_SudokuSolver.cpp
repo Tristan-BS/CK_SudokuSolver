6b8cfe985c092f67c4c63b9c06002d78cfb6e0f1
Tristan-BS <98057075+Tristan-BS@users.noreply.github.com>
2024-11-14 13:14:37 +0100

    Changelog 14.11.2024 - 13:14
    -> Comment to whole Code clear

--- CK_SudokuSolver/CK_SudokuSolver.cpp
+++ CK_SudokuSolver/CK_SudokuSolver.cpp
@@ -6,6 +6,8 @@
 
 using namespace std;
 
+                                        // Function to check if it is safe to place a number in a cell
+
 bool isSafe(vector<vector<int>>& grid, int row, int col, int num) {
     for (int x = 0; x < N; x++) {
         if (grid[row][x] == num || grid[x][col] == num) {
@@ -23,52 +25,59 @@ bool isSafe(vector<vector<int>>& grid, int row, int col, int num) {
     return true;
 }
 
+                                        // Solves the Sudoku grid using backtracking
+
 bool solveSudoku(vector<vector<int>>& grid, int row, int col) {
-    if (row == N - 1 && col == N) {
+	if (row == N - 1 && col == N) {                     // If we have reached the end of the grid
         return true;
     }
-    if (col == N) {
+	if (col == N) {                                     // If we have reached the end of a row
         row++;
         col = 0;
     }
-    if (grid[row][col] != UNASSIGNED) {
-        return solveSudoku(grid, row, col + 1);
+	if (grid[row][col] != UNASSIGNED) {                 // If the cell is already filled
+		return solveSudoku(grid, row, col + 1);         // Move to the next cell
     }
-    for (int num = 1; num <= N; num++) {
-        if (isSafe(grid, row, col, num)) {
-            grid[row][col] = num;
-            if (solveSudoku(grid, row, col + 1)) {
+	for (int num = 1; num <= N; num++) {                // Try placing a number in the cell
+		if (isSafe(grid, row, col, num)) {              // Check if it is safe to place the number
+			grid[row][col] = num;                       // Place the number
+			if (solveSudoku(grid, row, col + 1)) {      // Recursively solve the rest of the grid
                 return true;
             }
-            grid[row][col] = UNASSIGNED;
+			grid[row][col] = UNASSIGNED;                // If the number doesn't lead to a solution, backtrack
+			                                            // Backtracking = Reset the cell to UNASSIGNED and try the next number
         }
     }
     return false;
 }
 
-void printGrid(vector<vector<int>>& grid) {
-    cout << "\n\n\n" << "Solution:" << endl;
-    for (int row = 0; row < N; row++) {
-        for (int col = 0; col < N; col++) {
-            cout << grid[row][col] << " ";
+                                        // Function to print the Sudoku grid
+
+void printGrid(vector<vector<int>>& grid) {             // Function to print the Sudoku grid
+	cout << "\n\n\n" << "Solution:" << endl;            // Print the solution
+	for (int row = 0; row < N; row++) {                 // Traverse each row
+		for (int col = 0; col < N; col++) {             // Traverse each column
+			cout << grid[row][col] << " ";              // Print the cell value
         }
         cout << endl;
     }
 }
 
+                                        // Main Function at the beginning of the program
+
 int main() {
-    vector<vector<int>> grid(N, vector<int>(N));
-    cout << "Enter the Sudoku grid (use 0 for empty cells):" << endl;
-    for (int row = 0; row < N; row++) {
-        for (int col = 0; col < N; col++) {
+	vector<vector<int>> grid(N, vector<int>(N));
+	cout << "Enter the Sudoku grid (use 0 for empty cells):" << endl;
+	for (int row = 0; row < N; row++) {
+		for (int col = 0; col < N; col++) {
             cin >> grid[row][col];
         }
     }
 
-    if (solveSudoku(grid, 0, 0)) {
-        printGrid(grid);
+	if (solveSudoku(grid, 0, 0)) {              // If True is returned by solveSudoku function
+		printGrid(grid);
     }
-    else {
+	else {                                      // If False is returned by solveSudoku function
         cout << "No solution exists" << endl;
     }
 
